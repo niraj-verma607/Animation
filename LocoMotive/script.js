87de145d7405de3568bf56eb5622393216c3ee4a
niraj-verma607 <nirajverma9607@gmail.com>
2026-07-08 19:25:47 +0530

    commit

--- LocoMotive/script.js
+++
@@ -1,6 +0,0 @@
-//Copy and paste from (https://github.com/locomotivemtl/locomotive-scroll) locomotive github in smooth section js section
-
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
